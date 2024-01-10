8c076fefa9fc07d0de71cd8e5f669f550d2cfbeb
DucNS <sonducn1412@gmail.com>
2024-01-10 11:27:24 +0700

    first init

--- src/main/java/edu/hust/factory/config/CacheConfiguration.java
+++ src/main/java/edu/hust/factory/config/CacheConfiguration.java
@@ -48,6 +48,7 @@ public class CacheConfiguration {
             createCache(cm, edu.hust.factory.domain.User.class.getName());
             createCache(cm, edu.hust.factory.domain.Authority.class.getName());
             createCache(cm, edu.hust.factory.domain.User.class.getName() + ".authorities");
+            createCache(cm, edu.hust.factory.domain.Employee.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'employee',
+    data: { pageTitle: 'factoryApp.employee.home.title' },
+    loadChildren: () => import('./employee/employee.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
